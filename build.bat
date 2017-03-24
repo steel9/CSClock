2bf59de987be4c175ffc997794bcf920386e47f9
Viktor <Viktor@VIKTOR-GAMING>
2017-03-24 20:10:49 +0100

    fix

--- build.bat
+++ build.bat
@@ -1,3 +1,20 @@
+@REM 		CSClock - a program which keeps track of your computer time
+@REM 		Copyright (C) 2017  Viktor J
+
+@REM 		This program is free software: you can redistribute it and/or modify
+@REM 		it under the terms of the GNU General Public License as published by
+@REM 		the Free Software Foundation, either version 3 of the License, or
+@REM 		(at your option) any later version.
+
+@REM 		This program is distributed in the hope that it will be useful,
+@REM 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+@REM 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+@REM		GNU General Public License for more details.
+
+@REM 		You should have received a copy of the GNU General Public License
+@REM		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 @echo off
 echo Building...
 cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
