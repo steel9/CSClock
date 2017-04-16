e0183963cc1f0dc453bd947287ddab89008dbc50
Viktor <Viktor@VIKTOR-GAMING>
2017-04-16 12:13:49 +0200

    0.2.2 bugfix

--- CSClock/SetOvertime.Designer.cs
+++ CSClock/SetOvertime.Designer.cs
@@ -67,10 +67,11 @@ namespace CSClock
             // 
             // label2
             // 
+            this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(13, 33);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(216, 13);
+            this.label2.Size = new System.Drawing.Size(120, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Set overtime (minutes)";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -135,6 +136,7 @@ namespace CSClock
             this.Load += new System.EventHandler(this.SetOvertime_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_maximumMinutesOvertime)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
