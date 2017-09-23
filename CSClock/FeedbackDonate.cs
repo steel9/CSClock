5401335b70d4ff032663e9ce6d091652c6f20355
Viktor <Viktor@VIKTOR-GAMING>
2017-09-23 19:40:17 +0200

    fix

--- CSClock/FeedbackDonate.cs
+++ CSClock/FeedbackDonate.cs
@@ -17,14 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CSClock
